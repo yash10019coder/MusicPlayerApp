0aa93dede1412efc1dd7dce5072967800eedbe31
yash10019coder <yashverma10019@gmail.com>
2024-03-15 20:14:48 +0530

    restructured the packages in app module.

---
+++ app/src/main/java/com/yash10019coder/musicplayerapp/ui/compose/songplayer/SongPlayerViewModel.kt
@@ -0,0 +1,8 @@
+package com.yash10019coder.musicplayerapp.compose.songplayer
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SongPlayerViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/yash10019coder/musicplayerapp/ui/compose/songslist/SongListViewModel.kt
@@ -0,0 +1,8 @@
+package com.yash10019coder.musicplayerapp.compose.songslist
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SongListViewModel : ViewModel() {
+}
