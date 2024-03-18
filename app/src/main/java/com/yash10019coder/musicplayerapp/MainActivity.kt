d2b92cfee52db5dd4ba8a953ad9b424387ff0714
yash10019coder <yashverma10019@gmail.com>
2024-03-19 02:04:17 +0530

    added navigation in the app. upgraded hilt to v2.51

--- app/src/main/java/com/yash10019coder/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/yash10019coder/musicplayerapp/MainActivity.kt
@@ -10,6 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yash10019coder.musicplayerapp.ui.compose.songplayer.SongPlayerScreen
+import com.yash10019coder.musicplayerapp.ui.compose.songslist.SongList
 import com.yash10019coder.musicplayerapp.ui.theme.MusicPlayerAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,13 +24,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+
             MusicPlayerAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                NavHost(
+                    navController = navController,
+                    startDestination = "songs"
                 ) {
-                    
+                    composable("songs") {
+                        SongList()
+                    }
                 }
             }
         }
